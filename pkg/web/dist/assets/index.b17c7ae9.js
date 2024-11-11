48ada6d16cac18311f6619f6007a6c251e9876f3
chenjipeng <2363008338@qq.com>
2024-11-11 15:27:18 +0800

    add dist

---
+++ pkg/web/dist/assets/index.b17c7ae9.js
@@ -0,0 +1,1 @@
+import{R as r,j as o}from"./index.d6bcc00a.js";import{E as e}from"./index.ec9d2343.js";const a=()=>o(e,{code:500});var t=r.memo(a);export{t as default};
---
+++ pkg/web/dist/assets/index.fe036315.js
@@ -0,0 +1,1 @@
+import{R as o,j as r}from"./index.d6bcc00a.js";import{E as a}from"./index.ec9d2343.js";const e=()=>r(a,{code:403});var s=o.memo(e);export{s as default};
